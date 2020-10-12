16e51fe0e6c715d312ac69c83dd9968206a68aec
tjreese90 <60903273+Tjreese@users.noreply.github.com>
2020-10-12 15:12:28 -0500

    update site

--- Dog Training Site/app.js
+++ Dog Training Site/app.js
@@ -25,7 +25,10 @@ const navSlides = () => {
 };
 navSlides();
 
-var mouse = { x: 0, y: 0 };
+var mouse = {
+  x: 0,
+  y: 0
+};
 var myWidth = 0,
   myHeight = 0;
 var mouseIsDown = false;
@@ -97,8 +100,8 @@ document.addEventListener(
       "px, circle, rgba(254,255,255,0.8) 5%,rgba(236,255,0,1) 10%,rgba(253,50,41,1) 25%, rgba(243,0,0,1) 40%,rgba(93,0,0,1) 100%)";
 
     document.getElementById(
-      "waterReflectionContainer"
-    ).style.perspectiveOrigin =
+        "waterReflectionContainer"
+      ).style.perspectiveOrigin =
       ((mouse.x / myWidth) * 100).toString() + "% -15%";
     document.getElementById("waterReflectionMiddle").style.left =
       (mouse.x - myWidth - myWidth * 0.03).toString() + "px";
@@ -253,3 +256,53 @@ function windowResize() {
   document.getElementById("waterDistance").style.height = myHeight - skyHeight;
   document.getElementById("division").style.top = skyHeight;
 }
+
+var Expand = (function () {
+  var tile = $('.strips__strip');
+  var tileLink = $('.strips__strip > .strip__content');
+  var tileText = tileLink.find('.strip__inner-text');
+  var stripClose = $('.strip__close');
+
+  var expanded = false;
+
+  var open = function () {
+
+    var tile = $(this).parent();
+
+    if (!expanded) {
+      tile.addClass('strips__strip--expanded');
+      // add delay to inner text
+      tileText.css('transition', 'all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)');
+      stripClose.addClass('strip__close--show');
+      stripClose.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
+      expanded = true;
+    }
+  };
+
+  var close = function () {
+    if (expanded) {
+      tile.removeClass('strips__strip--expanded');
+      // remove delay from inner text
+      tileText.css('transition', 'all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)');
+      stripClose.removeClass('strip__close--show');
+      stripClose.css('transition', 'all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)')
+      expanded = false;
+    }
+  }
+
+  var bindActions = function () {
+    tileLink.on('click', open);
+    stripClose.on('click', close);
+  };
+
+  var init = function () {
+    bindActions();
+  };
+
+  return {
+    init: init
+  };
+
+}());
+
+Expand.init();
